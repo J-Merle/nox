3d511fae30eab9b7b72bf87045a6b89b8aca13d6
Jeremy Merle <jeremy.merle@protonmail.com>
2024-12-18 20:23:03 +0100

    Add item struct and basic efficiency finding

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 #define STAT_ID_INTEL 0x7e
 #define STAT_ID_CRIT_CHANCE 0x73
 #define STAT_ID_STRENGTH 0x76
+#define STAT_ID_AGILITY 0x77
 #define STAT_ID_LUCK 0x7b
 #define STAT_ID_WISDOM 0x7c
 #define STAT_ID_VITA 0x7d
@@ -29,6 +30,8 @@
 #define STAT_ID_HUNTING 0x1b
 #define STAT_ID_DAMAGE_REFLECT 0xdc
 #define STAT_ID_DAMAGE_FIRE 0xa8
+#define STAT_ID_INIT 0xae
+#define STAT_ID_ESCAPE_DEBUF 0xf2
 
 #define DOFUS_SERVER_PORT 5555
 
@@ -40,12 +43,12 @@
 
 // Uncomment to get some raw data info as hexa
 //#define RAW_PACKET
-
 char* stat_name(uint8_t stat_id) {
     switch (stat_id) {
         case STAT_ID_INTEL: return "INTEL";
         case STAT_ID_CRIT_CHANCE: return "CRIT CHANCE";
         case STAT_ID_STRENGTH: return "STRENGTH";
+        case STAT_ID_AGILITY: return "AGILITY";
         case STAT_ID_VITA: return "VITA";
         case STAT_ID_LUCK: return "LUCK";
         case STAT_ID_WISDOM: return "WISDOM";
@@ -59,11 +62,56 @@ char* stat_name(uint8_t stat_id) {
         case STAT_ID_HUNTING: return "HUNTING";
         case STAT_ID_DAMAGE_REFLECT: return "DAMAGE REFLECT";
         case STAT_ID_DAMAGE_FIRE: return "DAMAGE FIRE";
+        case STAT_ID_INIT: return "INIT";
+        case STAT_ID_ESCAPE_DEBUF: return "DEBUF ESCAPE";
         default: 
             printf("%x was not found\n", stat_id);
             return "UNKNOWN";
     }
 }
+
+struct stat {
+    uint16_t code;
+    uint16_t value;
+};
+
+struct item {
+    struct stat stats[16];
+    uint8_t stats_len;
+    uint32_t price;
+    float efficiency;
+    uint16_t power;
+};
+
+void compute_item_infos(struct item* item) {
+    for (int i = 0; i < item->stats_len; i++) {
+
+        if(item->stats[i].code == STAT_ID_PA) item->power += 100*item->stats[i].value;
+        if(item->stats[i].code == STAT_ID_VITA) item->power += 1 * item->stats[i].value;
+        if(item->stats[i].code == STAT_ID_INTEL) item->power += 1 * item->stats[i].value;
+        if(item->stats[i].code == STAT_ID_STRENGTH) item->power += 1 * item->stats[i].value;
+        if(item->stats[i].code == STAT_ID_LUCK) item->power += 1 * item->stats[i].value;
+        if(item->stats[i].code == STAT_ID_AGILITY) item->power += 1 * item->stats[i].value;
+        if(item->stats[i].code == STAT_ID_DAMAGE_FIRE) item->power += 5 * item->stats[i].value;
+
+    }
+
+    if(item->price > 0) {
+        item->efficiency = (float) item->power / item->price;
+    }
+
+}
+void display_item_infos(struct item* item) {
+    printf("===================\n");
+    for(int i = 0; i < item->stats_len; i++) {
+        printf("%-20s\t%u\n", stat_name(item->stats[i].code), item->stats[i].value);
+    }
+    printf("Price %u K\n", item->price);
+    printf("Power: %d Efficiency %f\n", item->power, item->efficiency);
+    printf("===================\n");
+
+}
+
 
 uint8_t read_byte(unsigned char** data) {
     uint8_t value = *(*data);
@@ -187,7 +235,6 @@ int main(int argc, char* argv[]) {
       } 
 
       unsigned int data_size = (int)ntohs(ip->tot_len) - (int)(tcp->doff * 4) - (int)(ip->ihl * 4); // There is no easier way to determine data size ???
-      //printf("%d - %d - %d\n", (int)ntohs(ip->tot_len), (int)(tcp->doff * 4), (int)(ip->ihl * 4));
       if(data_size > 0) {
 
           // TODO Maybe we want to perform this operation only on the pakcets we are interrested in 
@@ -206,12 +253,13 @@ int main(int argc, char* argv[]) {
 
       char* type_string="type.ankama.com/";
       char data_type[4] = {0};
+      data_type[3] = '\0'; // Ensure we end the string (only usefull for printing the value)
 
       // We don't understand for now how the first bytes are used so we just search for the type string
       for( int i = 0; i < 30; i++) {
+
           if (memcmp(data, type_string, 16) == 0) {
               memcpy(data_type, data + 16, 3);
-            data_type[3] = '\0'; // Ensure we end the string (only usefull for printing the value)
               data += 16 + 3;
               break;
           }
@@ -220,6 +268,7 @@ int main(int argc, char* argv[]) {
 
       if(memcmp(data_type, "iyc", 3) == 0) {
           // Skip for hdv dev
+          payload_len = 0;
           continue;
           uint8_t value = *(data);
 
@@ -253,8 +302,9 @@ int main(int argc, char* argv[]) {
           value = read_byte(&data);
           value = read_var_short(&data);
           data += 2; // (Original item ID ?)
+          float best_efficiency = 0;
           while(data < payload + payload_len) { 
-              printf("===================\n");
+              struct item* current_item = malloc(sizeof (struct item)); // TODO remember to free this memory
               value = read_byte(&data);
               assert(value == 0x1a);
               // Lot of unknown data here
@@ -270,6 +320,9 @@ int main(int argc, char* argv[]) {
               value = read_byte(&data);
               assert(value == 0x22);
               int power = 0;
+              int stat_index = 0;
+
+
               while(value == 0x22) {
 
                   uint8_t stat_size = 0;
@@ -277,6 +330,8 @@ int main(int argc, char* argv[]) {
                   uint16_t stat_value = 0;
 
                   stat_size = read_byte(&data);
+
+
                   
                   // TODO
                   if(stat_size > 5) {
@@ -302,23 +357,30 @@ int main(int argc, char* argv[]) {
                   data++; // Skip next byte (0x18)
                   stat_value = read_var_short(&data);
 
-                  // TODO
-                  // Power is temporary computed here, will be moved
-                  if(stat_type == STAT_ID_PA) power += 100*stat_value;
-                  if(stat_type == STAT_ID_VITA) power += 1*stat_value;
-                  if(stat_type == STAT_ID_INTEL) power += 1*stat_value;
+                  struct stat current_stat = { .code = stat_type, .value = stat_value };
+                  current_item->stats[stat_index] = current_stat;
+                  current_item->stats_len++;
 
-                  printf("%-20s\t%u\n", stat_name(stat_type), stat_value);
+
                   value = read_byte(&data);
+                  stat_index++;
 #endif
               }
-              assert(value == MILESTONE_STAT_FOOTER);
+              
+              if(value != MILESTONE_STAT_FOOTER) {
+                  printf("Some data might be missing\n");
+                  break;
+              }
               value = read_byte(&data); // End block size
-              uint32_t price = read_var_int(&data);
+              current_item->price = read_var_int(&data);
+              compute_item_infos(current_item);
+              if(current_item->efficiency > best_efficiency) {
+
+                  best_efficiency = current_item->efficiency;
+                  printf("Better item found !\n"); // TODO in the future maybe store items in an array and sort it ?
+                  display_item_infos(current_item);
+              }
               data += 2; // Skip next two empty bytes
-              printf("Price %u K\n", price);
-              printf("Power: %d Efficiency %f\n", power, (float)power/price);
-              printf("===================\n");
           }
 
       } 
